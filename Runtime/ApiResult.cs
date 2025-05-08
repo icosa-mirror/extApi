cbc6696738c4e4c1f84b45ae5bcd21f477a8c38a
Andy Baker <andy@andybak.net>
2025-05-08 08:49:30 +0100

    Better json serialization. Allow result bodies for all response codes.

--- Runtime/ApiResult.cs
+++ Runtime/ApiResult.cs
@@ -1,20 +1,27 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using UnityEngine;
 
 namespace extApi
 {
     public class ApiResult
     {
-        public static ApiResult Ok() => new(HttpStatusCode.OK);
-        public static ApiResult Ok(object result) => new(HttpStatusCode.OK, result);
-        public static ApiResult BadRequest() => new(HttpStatusCode.BadRequest);
-        public static ApiResult NotFound() => new(HttpStatusCode.NotFound);
-        public static ApiResult InternalServerError() => new(HttpStatusCode.InternalServerError);
+        public static ApiResult Ok(object result = null) => new(HttpStatusCode.OK, result);
+        public static ApiResult BadRequest(object result = null) => new(HttpStatusCode.BadRequest, result);
+        public static ApiResult NotFound(object result = null) => new(HttpStatusCode.NotFound, result);
+        public static ApiResult InternalServerError(object result = null) => new(HttpStatusCode.InternalServerError, result);
 
         public HttpStatusCode StatusCode { get; }
         public string Json { get; }
 
-        private ApiResult(HttpStatusCode statusCode) => StatusCode = statusCode;
-        private ApiResult(HttpStatusCode statusCode, object result) : this(statusCode) => Json = JsonUtility.ToJson(result, false);
+        private ApiResult(HttpStatusCode statusCode)
+        {
+            StatusCode = statusCode;
+        }
+
+        private ApiResult(HttpStatusCode statusCode, object result) : this(statusCode)
+        {
+            Json = Newtonsoft.Json.JsonConvert.SerializeObject(result);
+        }
     }
 }
